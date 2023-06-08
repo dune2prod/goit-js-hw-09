88c20640ef5caeebccb6cddc252a909da17955e3
dune2prod <122452986+dune2prod@users.noreply.github.com>
2023-06-08 13:07:41 +0000

    Deploying to gh-pages from @ dune2prod/goit-js-hw-09@0e7deda26a0dcba907d2a5259b1ef2ba1dec1abc 🚀

---
+++ 01-color-switcher.360e1d11.js
@@ -0,0 +1,2 @@
+console.log(document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)));
+//# sourceMappingURL=01-color-switcher.360e1d11.js.map
---
+++ 01-color-switcher.aed6cde3.js
@@ -0,0 +1,2 @@
+console.log(document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`);
+//# sourceMappingURL=01-color-switcher.aed6cde3.js.map
